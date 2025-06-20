1aa074e60b365fc59f29a9c38d1145df7de1f68a
Shivaanjay Narula <shivaanjaynarula@gmail.com>
2025-06-20 13:27:42 +0530

    Create 5) Sort an array of 0's, 1's and 2's.java

---
+++ Day-1: Arrays/5) Sort an array of 0's, 1's and 2's.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+        int left = 0;
+        int mid = 0;
+        int right = n - 1;
+        while(mid <= right)
+        {
+            if(nums[mid] == 0)
+            {
+                swap(nums, left++, mid++);
+            }
+            else if (nums[mid] == 1)
+            {
+                mid++;
+            }
+            else
+            {
+                swap(nums, mid, right--);
+            }
+        }
+    }
+    private void swap(int[] nums, int i, int j)
+    {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
